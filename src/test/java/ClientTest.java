992a5b067f1c07a9a7c21523a504a46f7f596259
[Tranqu1lo] <[andregois000@gmail.com]>
2023-05-08 17:36:51 +0100

    test for  RSA,MESSAGE,DifieHelman,Mac,ByteUtil,AES,Hash,Digest,FileHandler,Server,RequestUtils

---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,73 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientTest {
+    private     Client client;
+    @BeforeEach
+    void setUp() throws Exception {
+        client = new Client ( 8000 );
+    }
+
+    @Test
+    void getClientName() {
+
+    }
+
+    @Test
+    void setClientName() {
+    }
+
+    @Test
+    void setPrivateKey() {
+    }
+
+    @Test
+    void setPublicKey() {
+    }
+
+    @Test
+    void getPrivateKey() {
+    }
+
+    @Test
+    void getPublicKey() {
+    }
+
+    @Test
+    void setFileName() {
+    }
+
+    @Test
+    void getFileName() {
+    }
+
+    @Test
+    void setConnected() {
+    }
+
+    @Test
+    void doHandshake() {
+    }
+
+    @Test
+    void sendClientChoice() {
+    }
+
+    @Test
+    void endConnection() {
+    }
+
+    @Test
+    void saveFiles() {
+    }
+
+    @Test
+    void sendMessage() {
+    }
+
+    @Test
+    void execute() {
+    }
+}
