c792f7be55c18006ebe27573c00729e11e011e48
[Tranqu1lo] <[andregois000@gmail.com]>
2023-05-08 19:28:13 +0100

    fix : working

--- src/test/java/HashTest.java
+++ src/test/java/HashTest.java
@@ -1,43 +1,39 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.security.MessageDigest;
 import java.util.Arrays;
 
 public class HashTest {
-    private String message ="test message";
-    private byte[] messageBytes = message.getBytes();
+
     @Test
-    public void testGenerateDigest() {
-        try {
-            byte[] digest = Hash.generateDigest(messageBytes);
-            Assertions.assertNotNull(digest);
-            Assertions.assertTrue(digest.length > 0);
-        } catch (Exception e) {
-            Assertions.fail("An exception occurred while generating the digest: " + e.getMessage());
-        }
+    void testGenerateDigest() throws Exception {
+        // Test with empty message
+        byte[] message = new byte[0];
+        byte[] digest = Hash.generateDigest(message, "SHA-256");
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] expectedDigest = md.digest(message);
+        Assertions.assertArrayEquals(expectedDigest, digest);
+
+        // Test with non-empty message
+        message = "Hello World".getBytes();
+        digest = Hash.generateDigest(message, "MD5");
+        md = MessageDigest.getInstance("MD5");
+        expectedDigest = md.digest(message);
+        Assertions.assertArrayEquals(expectedDigest, digest);
     }
 
     @Test
-    public void testVerifyDigest() {
-        try {
-            byte[] digest = Hash.generateDigest(messageBytes);
-            boolean isValid = Hash.verifyDigest(digest, digest);
-            Assertions.assertTrue(isValid);
-        } catch (Exception e) {
-            Assertions.fail("Error " + e.getMessage());
-        }
+    void testVerifyDigest() {
+        // Test with valid digest
+        byte[] digest = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3".getBytes();
+        byte[] computedDigest = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3".getBytes();
+        Assertions.assertTrue(Hash.verifyDigest(digest, computedDigest));
+
+        // Test with invalid digest
+        digest = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3".getBytes();
+        computedDigest = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd4".getBytes();
+        Assertions.assertFalse(Hash.verifyDigest(digest, computedDigest));
     }
 
-    @Test
-    public void testVerifyDigestDifferent() {
-        byte[] messageBytes = message.getBytes();
-        try {
-            byte[] digest = Hash.generateDigest(messageBytes);
-            byte[] differentDigest = Arrays.copyOf(digest, digest.length);
-            differentDigest[differentDigest.length - 1] = (byte) ~differentDigest[differentDigest.length - 1]; // invert last byte
-            boolean isValid = Hash.verifyDigest(digest, differentDigest);
-            Assertions.assertFalse(isValid);
-        } catch (Exception e) {
-            Assertions.fail("Error" + e.getMessage());
-        }
-    }
 }
