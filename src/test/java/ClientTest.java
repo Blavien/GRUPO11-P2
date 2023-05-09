ff3537c705e7fa29d2403c0da8c7976778dc536b
[pvpkiller01] <[henrique2777.vasconcelos98@gmail.com]>
2023-05-09 20:33:47 +0100

    Unit tests and few modifications in Client

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,5 +1,10 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.nio.file.Files;
@@ -8,7 +13,8 @@ import java.nio.file.Paths;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-
+import java.util.ArrayList;
+import org.apache.commons.io.IOUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,29 +29,54 @@ class ClientTest {
     @Test
     void setGetClientName() throws Exception {
         serverThread.start ( );
-        Client client = new Client ( 8000,true );
+
+        InputStream sysInBackup = System.in;
+        ByteArrayInputStream in = new ByteArrayInputStream("francisco".getBytes());
+        System.setIn(in);
+
+
+        Client client = new Client ( 8000);
+        assertEquals("francisco",client.getClientName());
+
+
         client.setClientName("joao");
         assertEquals("joao",client.getClientName());
+        System.setIn(sysInBackup);
     }
 
 
     @Test
     void setGetPrivateKey() throws Exception {
         serverThread.start ( );
-        Client client = new Client ( 8000,true );
+
+        InputStream sysInBackup = System.in;
+        ByteArrayInputStream in = new ByteArrayInputStream("francisco".getBytes());
+        System.setIn(in);
+
+
+        Client client = new Client ( 8000);
         PrivateKey bomdiaKey = RSA.getPrivateKey(client.getClientName());
         client.setPrivateKey();
         assertEquals(bomdiaKey, client.getPrivateKey());
+
+        System.setIn(sysInBackup);
     }
 
     @Test
     void setGetPublicKey() throws Exception {
         serverThread.start ( );
-        Client client = new Client ( 8000,true );
+
+        InputStream sysInBackup = System.in;
+        ByteArrayInputStream in = new ByteArrayInputStream("francisco".getBytes());
+        System.setIn(in);
+
+
+        Client client = new Client ( 8000);
         PublicKey bomdiaKey = RSA.getPublicKey(client.getClientName());
         client.setPublicKey();
         assertEquals(bomdiaKey, client.getPublicKey());
 
+        System.setIn(sysInBackup);
     }
 
 
@@ -53,9 +84,16 @@ class ClientTest {
     @Test
     void setGetFileName() throws Exception {
         serverThread.start ( );
-        Client client = new Client ( 8000,true );
+
+        InputStream sysInBackup = System.in;
+        ByteArrayInputStream in = new ByteArrayInputStream("francisco".getBytes());
+        System.setIn(in);
+
+        Client client = new Client ( 8000);
         client.setFileName("ola");
         assertEquals("ola", client.getFileName());
+
+        System.setIn(sysInBackup);
 
     }
 
@@ -67,22 +105,40 @@ class ClientTest {
 
     //nao faço
     @Test
-    void doHandshake() {
+    void doHandshake() throws Exception {
+
+        serverThread.start ( );
+
+        InputStream sysInBackup = System.in;
+        ByteArrayInputStream in1 = new ByteArrayInputStream("francisco\n0\n0".getBytes());
+        System.setIn(in1);
+
+
+
+        Client client = new Client ( 8000);
+        assertEquals("francisco", client.getClientName());
+        RequestUtils.writeNumberToFile(1,RequestUtils.HANDSHAKE_SIGNAL);
+        assertEquals(false,client.doHandshake());
+
+        System.setIn(sysInBackup);
+
+
     }
 
-    @Test
-    void sendClientChoice() {
-    }
 
-    @Test
-    void endConnection() {
-    }
+
+
 
     @Test
     void saveFiles() throws Exception {
 
         serverThread.start ( );
-        Client client = new Client ( 8000,true );
+
+        InputStream sysInBackup = System.in;
+        ByteArrayInputStream in = new ByteArrayInputStream("francisco".getBytes());
+        System.setIn(in);
+
+        Client client = new Client ( 8000);
         client.setClientName("joao");
         client.setFileName("boatarde.txt");
         client.saveFiles("aqui");
@@ -96,20 +152,34 @@ class ClientTest {
         String conteudoArquivo = new String(bytesArquivo, StandardCharsets.UTF_8);
         assertEquals("aqui",conteudoArquivo);
 
+
+        System.setIn(sysInBackup);
+
     }
 
     @Test
-    void sendMessage() throws Exception {
+    void sendRequestAndWritingFile() throws Exception {
+
+        InputStream sysInBackup = System.in;
+
         serverThread.start ( );
-        Client client = new Client ( 8000,false );
+        String initialString = "francisco\n0\n0";
+        InputStream targetStream = IOUtils.toInputStream(initialString);
+        System.setIn(targetStream);
+        Client client = new Client ( 8000);
+
+        RequestUtils.writeNumberToFile(1,RequestUtils.HANDSHAKE_SIGNAL);
+
+        client.doHandshake();
+
+        client.execute("ola.txt");
+        String textoOriginal = new String(Files.readAllBytes(Paths.get("server/files/ola.txt")));
+        String texto = new String(Files.readAllBytes(Paths.get("francisco/files/User_ola.txt")));
 
 
-        client.sendMessage("hahaha");
-
-        assertEquals("hahaha",ClientHandler.getMessageTest());
+        assertEquals(textoOriginal,texto);
+        System.setIn(sysInBackup);
     }
 
-    @Test
-    void execute() {
-    }
+
 }
